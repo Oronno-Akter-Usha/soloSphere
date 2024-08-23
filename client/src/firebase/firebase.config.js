8a1c2f8169baf6ef848a3c504889a70505d62ddf
Oronno Akter Usha <ushaoronno1@gmail.com>
2024-08-23 22:53:14 +0600

    added the navbar and images

---
+++ client/src/firebase/firebase.config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAAuN_XmI-wSPnNJdexK6K-uTgTn90P29Q",
+  authDomain: "solosphere-f1c90.firebaseapp.com",
+  projectId: "solosphere-f1c90",
+  storageBucket: "solosphere-f1c90.appspot.com",
+  messagingSenderId: "210613723970",
+  appId: "1:210613723970:web:503348185c45e4909fdedb",
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
---
+++ client/src/layouts/Main.jsx
@@ -0,0 +1,15 @@
+import Navbar from "./../components/Navbar";
+
+const Main = () => {
+  return (
+    <div>
+      {/* Navbar */}
+      <Navbar />
+      {/* Outer */}
+
+      {/* Footer */}
+    </div>
+  );
+};
+
+export default Main;
